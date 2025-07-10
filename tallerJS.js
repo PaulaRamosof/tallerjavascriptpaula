727bdbf6f2423e42baa73c2dc58f9dd331e4a1c9
Paularamosof <paulaandrearamospinzon@gmail.com>
2025-07-10 16:16:51 -0500

    Taller JS listo y funcional

---
+++ tallerJS.js
@@ -0,0 +1,14 @@
+//  Variables y Tipos de Datos:
+let nombre = "Paula Ramos" ;
+let edad = 32 ;
+let esEstudiante = true;
+
+let sinValor;
+let valorNulo = null;
+
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("¿Es estudiante?", esEstudiante);
+console.log("Sin valor:", sinValor);
+console.log("Valor nulo:", valorNulo);
