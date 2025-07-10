14968985a41f9878e9a9fb816fbbd0dcb7075f94
Paularamosof <paulaandrearamospinzon@gmail.com>
2025-07-10 17:36:26 -0500

    Mejora en validaciones del cálculo de edad

---
+++ Ejercicio2.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+let anionacimiento = Number(prompt("Ingrese su fecha de nacimiento"));
+let anioactual = new Date().getFullYear();
+let actual= anioactual - anionacimiento;
+
+console.log("su edad es,",actual,"Años")
